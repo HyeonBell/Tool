86782ca04f3f7e111346a02015bc5bc501e96af1
HyeonBell <hyeonbells@gmail.com>
2021-06-17 15:41:55 +0900

    python simple tool add

---
+++ python/read_file_and_delete_line/read_file_and_delete_line.py
@@ -0,0 +1,27 @@
+#-*- coding: utf-8 -*-
+import time
+
+def read_file_and_delete_line():
+    global anwser_request_box
+    time.sleep(1)
+    read_f = open("target.txt", "r")
+    check = read_f.read()
+    save_data = str(check[:check.find('\n')+1])
+
+    write_f = open("target.txt", "w")
+    write_f.write(check.replace(check[:check.find('\n')+1], ''))
+    write_f.close()
+    read_f.close()
+
+    if check == '':
+        pass
+    else:
+        print(save_data)
+        pass
+
+
+while True:
+    read_file_and_delete_line()
+    time.sleep(5)
+
+
